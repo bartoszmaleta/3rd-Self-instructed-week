007bb19483957df4ea2e7083b97d6b1a9fd6a555
Bartosz Maleta <bartosz.maleta@gmail.com>
2019-10-27 13:44:00 +0100

    Updated names of classes

--- inventory_game/stable_game_inventory_6.1.1.py
+++ inventory_game/stable_game_inventory_6.1.1.py
@@ -2,7 +2,10 @@ import operator
 import csv
 
 print()
+print('\n-------------------------------------------- ')
 print('\n-------------------------------------------- Step 1.0.')
+step1 = 'Step 1. \nWrite a function named display_inventory(inventory) that would take any possible “inventory” and display it \n'
+print(step1)
 
 inv = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}  
 dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
@@ -34,11 +37,15 @@ def display_inventory(inventory):
     display_total_number_of_inventory()
 
 
-print('\n-------------------------------------------- ')
-
 display_inventory(inv) 
-
+print('\n-------------------------------------------- ')
 print('\n-------------------------------------------- Step 2.0.')
+step2_1 = 'Step 2. \nWrite a function named add_to_inventory(inventory, added_items) , where the inventory '
+step2_2 = 'parameter is a dictionary representing the player’s inventory and the added_items parameter is a list like dragon_loot. '
+step2_3 = 'The add_to_inventory() function should return a dictionary that represents the updated inventory. \n'
+print(step2_1)
+print(step2_2)
+print(step2_3)
 
 dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
 
@@ -53,33 +60,69 @@ def add_to_inventory(inventory, added_items):
 
 add_to_inventory(inv, dragon_loot)
 display_inventory(inv)
-
+print('\n-------------------------------------------- ')
 print('\n-------------------------------------------- Step 3.0.')
+step2_1 = 'Step 3. \nWrite a function named print_table(inventory, order) that takes your inventory and  '
+step2_2 = 'displays it in a well-organized table with each column right-justified. '
+step2_3 = 'The input argument is an order parameter (string), which works as the following: '
+step2_4 = ' - empty (by default) means the table is unordered'
+step2_5 = ' - "count,desc" means the table is ordered by count (of items in the inventory) in descending order'
+step2_6 = ' - "count,asc" means the table is ordered by count in ascending order \n'
+print(step2_1)
+print(step2_2)
+print(step2_3)
+print(step2_4)
+print(step2_5)
+print(step2_6)
 
 
 def print_table(inventory, order=None):
     space = ' '
     vertical_line = '|'
     dash = '-'
+    longest_string_in_inv = max(len(longest_string) for longest_string in inv)  # =9
+
+    number_of_dashes = longest_string_in_inv + 3 + len('count')
+    dashes = dash * number_of_dashes  # =17
     
     # if order is issubclass(int, str):
     #     print('asqwe')
 
     if order is not None:
-        print('Choose correct order. You can choose between ascending and descending.')
+        print('Choose order. You can choose between ascending and descending.')
         print("To display ascending table write: 'print_table(inv, 'count,asc')")
         print("To display descending table write: 'print_table(inv, 'count,desc')")
+        print('\n Your inventory: \n')
 
     if order is None:
         print('You does not enter order. You can choose between ascending and descending.')
         print("To display ascending table write: 'print_table(inv, 'count,asc')")
         print("To display descending table write: 'print_table(inv, 'count,desc')")
+        print('Right now the table will be printed without order!')
+        # inv = order
+        # asc_sorted_inv = sorted(inv.items(), key=operator.itemgetter(-1))
+        print(dashes)
+        print("{:<10} {:<15}".format('item_name |', 'count'))
+        print(dashes)
+        for keys, values in inv.items():
+            # count = values
+            
+            number_of_whitespaces_before_item_name = longest_string_in_inv - len(keys)
+            whitespaces_before_item_name = ' ' * number_of_whitespaces_before_item_name
+            keys_with_spaces_and_line = whitespaces_before_item_name + keys + space + vertical_line
+            
+            values_str = str(values)
+            length_of_count = len('count')
+            number_of_whitespaces_before_count = length_of_count - len(values_str)
+            whitespaces_before_count = ' ' * number_of_whitespaces_before_count
+            count_with_spaces = whitespaces_before_count + values_str
 
-    longest_string_in_inv = max(len(longest_string) for longest_string in inv)  # =9
-
-    number_of_dashes = longest_string_in_inv + 3 + len('count')
-    dashes = dash * number_of_dashes  # =17
-
+            print("{:<0} {:<15}".format(keys_with_spaces_and_line, count_with_spaces))
+        
+        print(dashes)
+        print()
+        display_total_number_of_inventory()  
+        
     if order == 'count,desc':
         desc_sorted_inv = order
         desc_sorted_inv = sorted(inv.items(), key=operator.itemgetter(-1), reverse=True)
@@ -132,6 +175,13 @@ def print_table(inventory, order=None):
 
 
 print()
+print('++++++++++++++++')
+print('No order: ')
+print()
+print_table(inv)
+print()
+print('++++++++++++++++')
+print()
 print('Descending order: ')
 print()
 print_table(inv, 'count,desc')
@@ -141,7 +191,17 @@ print('Ascending order: ')
 print()
 print_table(inv, 'count,asc')
 
+print('\n-------------------------------------------- ')
 print('\n-------------------------------------------- Step 4.0.')
+step2_1 = 'Step 4. \nWrite a function named import_inventory(inventory, filename) which can import new inventory items  '
+step2_2 = 'from a file - the filename comes as an argument, but by default it is import_inventory.csv. The import '
+step2_3 = 'automatically merges items by name. The file format is plain text with comma separated values (CSV).  '
+step2_4 = 'Example file content:'
+step2_5 = 'ruby,rope,ruby,gold coin,ruby,axe \n'
+print(step2_1)
+print(step2_2)
+print(step2_3)
+print(step2_4)
 
 
 def import_inventory(inventory, filename='/home/acer/Documents/3rd-Self-instructed-week/3rd-Self-instructed-week/import_inventory.csv'):
@@ -168,7 +228,15 @@ def import_inventory(inventory, filename='/home/acer/Documents/3rd-Self-instruct
 import_inventory(inv)
 
 
+print('\n-------------------------------------------- ')
 print('\n-------------------------------------------- Step 5.0.')
+step2_1 = 'Step 5. \nWrite a function named export_inventory(inventory, filename) which can export all inventory items to a file.   '
+step2_2 = 'The filename comes as an argument - if not, it automatically creates (and overwrites) the file '
+step2_3 = 'called export_inventory.csv. The file format is the same plain text with comma separated values (CSV). \n'
+print(step2_1)
+print(step2_2)
+print(step2_3)
+
 print(inv)
     
 
@@ -204,7 +272,13 @@ def export_inventory(inventory, filename):
 exported_file_name = "export_inventory.csv"
 export_inventory(inv, exported_file_name)
 
+print('\n-------------------------------------------- ')
 print('\n-------------------------------------------- Step 6.1.1')
+step2_1 = 'Step 6. \nEnsure your functions are working with specially named items,  '
+step2_2 = 'which have a unicode character or accent in their name or other special characters (especially space, tab). \n'
+print(step2_1)
+print(step2_2)
+
 
 monster_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
 
